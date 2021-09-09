65d06e411293652022d5a2b1d039f41ab5bb403a
Chris Zichko <chriszichkocoding@gmail.com>
2021-09-09 12:51:02 -0400

    Adding some function to the edit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,9 +97,15 @@ var createTaskActions = function(taskId) {
 formEl.addEventListener("submit", taskFormHandler);
 
 var taskButtonHandler = function(event) {
-    console.log(event.target);
+    var targetEl = event.target;
 
-    if (event.target.matches(".delete-btn")) {
+    // edit button was clicked
+    if (targetEl.matches(".edit-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+    // delete button was clicked
+    else if (targetEl.matches(".delete-btn")) {
         // get the element's task id
         var taskId = event.target.getAttribute("data-task-id");
         deleteTask(taskId);
@@ -111,8 +117,26 @@ var deleteTask = function(taskId) {
     taskSelected.remove();
 }
 
+var editTask = function(taskId) {
+    console.log("editing task #" + taskId);
+
+    // get task list item element
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // get content from task name and type
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    
+
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+
+
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type'").value = taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+    formEl.setAttribute("data-task-id", taskId);
+}
+
 
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 
-// #################### Error happening on line 110 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
