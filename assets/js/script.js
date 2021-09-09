b7ed22d9b447e95e96d477e393081a77f71368ef
Chris Zichko <chriszichkocoding@gmail.com>
2021-09-08 23:51:41 -0400

    woking on delete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function(event) {
     event.preventDefault();
@@ -42,7 +43,7 @@ var createTaskEl = function(taskDataObj) {
 
     var taskActionsEl = createTaskActions(taskIdCounter);
     listItemEl.appendChild(taskActionsEl);
-    
+
     // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 
@@ -94,3 +95,24 @@ var createTaskActions = function(taskId) {
 
 
 formEl.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+
+    if (event.target.matches(".delete-btn")) {
+        // get the element's task id
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id'" + taskId + "']");
+    console.log(taskSelected);
+}
+
+
+formEl.addEventListener("submit", taskFormHandler);
+pageContentEl.addEventListener("click", taskButtonHandler);
+
+// #################### Error happening on line 110 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
